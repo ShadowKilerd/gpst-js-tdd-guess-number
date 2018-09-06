0ad25b780ef24683aaed2b4ec7dee400510c9e26
Xin Chen <xinszchen@thoughtworks.com>
2018-09-06 23:51:11 +0800

    feat: more test case for judge

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -4,4 +4,10 @@ describe('judge', () => {
     it('should return 4b when given 1234 and 5678', function () {
         expect(judge('1234', '5678')).toBe('0A4B');
     });
+    it('should return 1A3b when given 1234 and 4567', function () {
+        expect(judge('1234', '4567')).toBe('1A3B');
+    });
+    it('should return 4A0B when given 1234 and 1234', function () {
+        expect(judge('1234', '1234')).toBe('4A0B');
+    });
 });
