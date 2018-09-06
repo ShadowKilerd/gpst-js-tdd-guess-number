8b9687547f8ba89b73eb160334309504e6ca8456
Xin Chen <xinszchen@thoughtworks.com>
2018-09-06 23:50:11 +0800

    feat: implement judge function logic

--- main/main.js
+++ main/main.js
@@ -1,6 +1,11 @@
 // Write your cade below:
-function main() {};
+function judge(randomNumber, userGuessedNumber) {
+    const randomNumberArray = randomNumber.split("");
+    const matchedNumber = userGuessedNumber.split("").reduce((sum, number) => (randomNumberArray.includes(number) ? sum + 1 : sum), 0)
+
+    return `${matchedNumber}A${4-matchedNumber}B`
+};
 
 module.exports = {
-    main
+    judge
 }
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,7 @@
-const main = require('../main/main');
+const {judge} = require('../main/main');
 
-describe('main()', () => {
-
+describe('judge', () => {
+    it('should return 4b when given 1234 and 5678', function () {
+        expect(judge('1234', '5678')).toBe('0A4B');
+    });
 });
